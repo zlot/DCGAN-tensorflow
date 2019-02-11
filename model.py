723c3d918a4e72c05a011a1f46d6810790d82eaf
Mark C Mitchell <mark.mitchell.00@gmail.com>
2019-02-12 10:58:20 +1100

    Update model.py to save_images at our specified exact samples_dir, not prepending the samples_dir with ./
    This is causing problems with my paperspace job runner, when trying to save images to /storage or /artifacts.

--- model.py
+++ model.py
@@ -297,7 +297,7 @@ class DCGAN(object):
               }
             )
             save_images(samples, image_manifold_size(samples.shape[0]),
-                  './{}/train_{:02d}_{:04d}.png'.format(config.sample_dir, epoch, idx))
+                  '{}/train_{:02d}_{:04d}.png'.format(config.sample_dir, epoch, idx))
             print("[Sample] d_loss: %.8f, g_loss: %.8f" % (d_loss, g_loss)) 
           else:
             try:
@@ -309,7 +309,7 @@ class DCGAN(object):
                 },
               )
               save_images(samples, image_manifold_size(samples.shape[0]),
-                    './{}/train_{:02d}_{:04d}.png'.format(config.sample_dir, epoch, idx))
+                    '{}/train_{:02d}_{:04d}.png'.format(config.sample_dir, epoch, idx))
               print("[Sample] d_loss: %.8f, g_loss: %.8f" % (d_loss, g_loss)) 
             except:
               print("one pic error!...")
